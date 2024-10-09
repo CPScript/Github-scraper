1b6babcb27e042e86750a6a3c4f918753824dbf1
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-09 13:53:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,16 @@
-import setuptools
+import os
+import time
+from os import system
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="Github-scraper",
-    version="1.2",
-    author="CPScript",
-    description="A script to scrape GitHub repositories from accounts",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/CPScript/Github-scraper",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "requests",
-        "beautifulsoup4"
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+# ugly asf
+print("installing:")
+print("""* requests
+* beautifulsoup4
+In 5 seconds...
+ctrl+C to stop
+""")
+time.sleep(6)
+os.system('pip install -r requeirments.txt -y')
+print("""
+Done, type `python main.py` to execute the script
+""")
